14a1c0bb6718c2399570525421a81ce88487bc10
YunSikKim <seneition@naver.com>
2025-05-24 02:45:35 +0900

    Sene-04 SceneSystem 추가
    - Const 클래스 추가
    - SceneChange 함수 추가 (Inspector 에서 Enum 값 변경으로 Scene 전환)

---
+++ Assets/_Scripts/Const.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const
+{
+    public static string TitleSceneName = "TitleScene";
+    public static string LobbySceneName = "LobbyScene";
+    public static string GateReadySceneName = "GateReadyScene";
+    public static string GateSceneName = "GateScene";
+    public static string BattleSceneName = "BattleScene";
+}
---
+++ Assets/_Scripts/Scene/SceneSystem.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework.Constraints;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSystem : MonoBehaviour
+{
+    public enum SceneType
+    {
+        Title = 0,
+        Lobby,
+        GateReady,
+        Gate,
+        Battle,
+        // ...
+    }
+
+    /**
+     * 특정 Scene 으로 전환하는 함수.
+     * Button 의 OnClick 에 바인딩하는 경우, SceneType Enum 값 변경으로 Scene 을 전환할 수 있게 한다.
+     */
+    [VisibleEnum(typeof(SceneType))]
+    public void ChangeScene(int _sceneType)
+    {
+        SceneType sceneType = (SceneType)_sceneType;
+
+        switch (sceneType)
+        {
+            case SceneType.Title:
+            {
+                SceneManager.LoadScene(Const.TitleSceneName);
+            }
+            break;
+            case SceneType.Lobby:
+            {
+                SceneManager.LoadScene(Const.LobbySceneName);
+            }
+            break;
+            case SceneType.GateReady:
+            {
+                SceneManager.LoadScene(Const.GateReadySceneName);
+            }
+            break;
+            case SceneType.Gate:
+            {
+                SceneManager.LoadScene(Const.GateSceneName);
+            }
+            break;
+            case SceneType.Battle:
+            {
+                SceneManager.LoadScene(Const.BattleSceneName);
+            }
+            break;
+            default:
+            break;
+        }
+    }
+}
